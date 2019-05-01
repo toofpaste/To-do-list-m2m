456b6b0b0f2520c7352e7c895b85a217488f829a
Darrion Gering and Liz Kelley <liz@>
2019-05-01 08:30:55 -0700

    add 2 tests

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,66 +1,96 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System;
 
 namespace ToDoList.Tests
 {
-   [TestClass]
-   public class ItemTest : IDisposable
-   {
+  [TestClass]
+  public class ItemTest : IDisposable
+  {
 
-       public void Dispose()
-       {
-           Item.ClearAll();
-       }
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
 
-       [TestMethod]
-       public void ItemConstructor_CreatesInstanceOfItem_Item()
-       {
-           Item newItem = new Item("stuff");
-           Assert.AreEqual(typeof(Item), newItem.GetType());
-       }
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("stuff");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
 
-       [TestMethod]
-       public void GetDescription_ReturnsDescription_String()
-       {
-           string description = "Walk the dog";
-           Item newItem = new Item(description);
-           string result = newItem.GetDescription();
-           Assert.AreEqual(description, result);
-       }
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog";
+      Item newItem = new Item(description);
+      string result = newItem.GetDescription();
+      Assert.AreEqual(description, result);
+    }
 
-       [TestMethod]
-       public void SetDescription_SetDescription_String()
-       {
-           string description = "walk the dog";
-           Item newItem = new Item(description);
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "walk the dog";
+      Item newItem = new Item(description);
 
-           string updatedDescription = "do the dishes";
-           newItem.SetDescription(updatedDescription);
-           string result = newItem.GetDescription();
+      string updatedDescription = "do the dishes";
+      newItem.SetDescription(updatedDescription);
+      string result = newItem.GetDescription();
 
-           Assert.AreEqual(updatedDescription, result);
-       }
+      Assert.AreEqual(updatedDescription, result);
+    }
 
-       [TestMethod]
-       public void GetAll_ReturnsEmptyList_ItemList()
-       {
-           List<Item> newList = new List<Item> { };
-           List<Item> result = Item.GetAll();
-           CollectionAssert.AreEqual(newList, result);
-       }
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item> { };
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
 
-       [TestMethod]
-       public void GetAll_ReturnsItems_ItemList()
-       {
-           string description01 = "walk the dog";
-           string description02 = "wash the dishes";
-           Item newItem1 = new Item(description01);
-           Item newItem2 = new Item(description02);
-           List<Item> newList = new List<Item> { newItem1, newItem2 };
-           List<Item> result = Item.GetAll();
-           CollectionAssert.AreEqual(newList, result);
-       }
-   }
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      string description01 = "walk the dog";
+      string description02 = "wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      int result = newItem.GetId();
+
+      //Assert
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+
+      //Act
+      Item result = Item.Find(2);
+
+      //Assert
+      Assert.AreEqual(newItem2, result);
+    }
+  }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,36 +1,49 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
-    public class Item
+  public class Item
+  {
+    private string _description;
+    private int _id;
+    private static List<Item> _instances = new List<Item> { };
+
+    public Item (string description)
     {
-        private string _description;
-        private static List<Item> _instances = new List<Item> { };
-
-        public Item (string description)
-        {
-            _description = description;
-            _instances.Add(this);
-        }
-
-        public string GetDescription()
-        {
-            return _description;
-        }
-
-        public void SetDescription(string newDescription)
-        {
-            _description = newDescription;
-        }
-
-        public static List<Item> GetAll()
-        {
-            return _instances;
-        }
-
-        public static void ClearAll()
-        {
-            _instances.Clear();
-        }
+      _description = description;
+      _instances.Add(this);
+      _id = _instances.Count;
     }
+
+    public string GetDescription()
+    {
+      return _description;
+    }
+
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+  }
 }
