202e08bf12f3a32d6c51bb82395d3a19f8c08242
Liz <jesusridesthebus@gmail.com>
2019-05-05 23:39:00 -0700

    add code to set up database

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,13 +5,11 @@ namespace ToDoList.Models
   public class Item
   {
     private string _description;
-    private int _id;
-    // private static List<Item> _instances = new List<Item> { };
+    // private int _id;
 
     public Item (string description)
     {
       _description = description;
-      // _instances.Add(this);
       // _id = _instances.Count;
     }
 
@@ -25,20 +23,37 @@ namespace ToDoList.Models
       _description = newDescription;
     }
 
+    // public int GetId()
+    // {
+    //   return _id;
+    // }
+
     public static List<Item> GetAll()
     {
       List<Item> allItems = new List<Item> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int itemId = rdr.GetInt32(0);
+        string itemDescription = rdr.GetString(1);
+        Item newItem = new Item(itemDescription, itemId);
+        allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
       return allItems;
     }
 
     // public static void ClearAll()
     // {
     //   _instances.Clear();
-    // }
-
-    // public int GetId()
-    // {
-    //   return _id;
     // }
 
     // public static Item Find(int searchId)
